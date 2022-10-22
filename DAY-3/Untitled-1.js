0153772f44b605ae5f33c4d4f754e9a45319ea87
Ovuzo <ovuzonyobeno@gmail.com>
2022-10-22 22:28:17 +0100

    added Day 3 and Day 4

---
+++ DAY-3/Untitled-1.js
@@ -0,0 +1,40 @@
+/**
+ Declare firstName, lastName, country, city, age, 
+ isMarried, year variable and assign value to it 
+ and use the typeof operator
+  to check different data types.
+ */
+
+  let firstName = "Tina";
+  let lastName = "James";
+  let country = "Nigeria";
+  let isMarried = true;
+  let year = 2022;
+
+  console.log(typeof(firstName))
+  console.log(typeof(lastName))
+  console.log(typeof(country))
+  console.log(typeof(isMarried))
+  console.log(typeof(year))
+
+  /**
+   2. Check if type of '10' is equal to 10
+   */
+
+   let num1 = "10";
+   let num2 = 10;
+   console.log(typeof(num1) == typeof(num2))
+
+   /**
+    Check if parseInt('9.8') is equal to 10
+    */
+
+    /*Boolean value is either true or false.
+
+    Write three JavaScript statement which provide truthy value.
+    Write three JavaScript statement which provide falsy value.*/
+
+    
+
+
+
---
+++ DAY-3/index.js
@@ -0,0 +1,9 @@
+/*Declare firstName, lastName, country, city, age, 
+isMarried, year variable and assign value to it and use 
+the typeof operator to check different data types.*/
+
+let firstName = "Tina";
+let lastName = "James";
+let country = "Nigeria";
+let city = "Ughelli";
+let age = 25;
